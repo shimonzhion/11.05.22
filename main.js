54c79ab978dfcadfa0df74b3c9672833caeee4bd
shimonzhion <brys94204@gmail.com>
2022-05-15 09:15:51 +0300

    11.05.22

---
+++ main.js
@@ -0,0 +1,135 @@
+var num = 0
+// while (num<101){
+//     console.log(num)
+//     num++
+// }
+// var i = 0;
+// var sum = 0;
+// while(i<5){
+//     var numbers = +prompt("enter number")
+//     sum += numbers;
+//     i++
+//     var avg = sum/i 
+  
+
+// }
+// console.log (avg)
+// console.log(sum)
+//  var i = 2 
+//  while (i<51){
+//      if (i%2==0){
+//          console.log(i)
+         
+//      }
+//      i++
+//  }
+// var i= 0
+// while(i<50){
+//     if (i%3 ==1){
+//         console.log(i)
+//     }
+//     i++
+// }
+// var fkt = 1
+// for (var i = prompt("lll");i>0 ; i--){
+//     fkt*=i
+  
+// }
+// document.write(fkt)
+
+
+// var i = +prompt("111");
+// var fkt= 1;
+// while(i>0){
+//     fkt*=i
+//     i--
+// }
+// document.write(fkt)
+// var fkt = 1
+// var i = +prompt("number")
+// while (i>0){
+//     fkt*i
+//     document.write(fkt)
+//     i--
+// }
+// var sum = +prompt("num")
+// var sum = 0;
+// var min = 0;
+// var sum = 0;
+// var i = 0;
+// while (i<5){
+//     var numbers = +prompt("num")
+//      i++
+
+  
+// }
+// document.write(numbers,max )
+// for (var i = 0 ; i<101 ; i++){
+//     console.log(i)
+// }
+// var sum = 0;
+// for (var i= 0 ; i<5 ; i++){
+//     var number = +prompt("enter number")
+//      sum+=number
+     
+
+// }
+// document.write(sum/i + "<br>")
+// document.write(sum)
+// for (var i = 0; i < 51; i++){
+//     if(i%2==0){
+//         document.write(i + "<br>")
+//     }
+
+// }
+
+// for (var i = 0; i<50 ; i++){
+//     if (i%3 == 1)
+//     console.log( i)
+// }
+// var i = 0;
+
+// do{
+// document.write(i+"<br>")
+// i++
+// }while(i<101)
+// var sum = 0;
+// var i = 0;
+// do{
+//     var numbers= +prompt("enter number")
+//     sum+=numbers
+//     i++
+    
+// }while(i<5)
+// document.write(sum +"<br>")
+// document.write(sum/i)
+
+// var i = 0; 
+// do{
+//     if (i%2==0){
+//         document.write(i)
+//     }
+//     i++
+// }while(i<51)
+// var i = 0 ;
+// do{
+//     if(i%3==1){
+//         document.write(i)
+//     }
+//     i++
+
+// }while(i<50)
+// var num = +prompt("enter num");
+// var sum = 1;
+// do{ 
+// sum*=num
+// num--
+// }while(num>1)
+// document.write(sum)
+// var num =  prompt("enter number"+"<br>")
+// var i =0;
+// while(i==0){
+  
+//   document.write(num)
+//   i++
+// }
